bb8dcf8c97e8e952ca9a128535a1618bf6cd9b5c
Johannes Sarpola <johannessarpola@users.noreply.github.com>
2018-01-14 17:40:26 +0200

    Minor renamings

--- src/main/kotlin/fi/johannes/data/services/proxy/package-info.java
+++ src/main/kotlin/fi/johannes/data/services/proxy/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Johannes on 9.1.2018.
  */
-@ModuleGen(groupPackage = "fi.johannes.data.proxy_vased", name = "wiki-database")
+@ModuleGen(groupPackage = "fi.johannes.data.services.proxy", name = "wiki-database")
 
 package fi.johannes.data.services.proxy;
 
